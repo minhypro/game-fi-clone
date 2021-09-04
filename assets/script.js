e07921b92d56c7515edf584bb515c0d535e7c518
Minh Y <le.minh.y.95@gmail.com>
2021-09-04 21:49:03 +0700

    update-animation

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,81 @@
 // window.addEventListener('scroll', function() {
 //     console.log("Scrollin'");
 //   });
+const ps5 = document.querySelector('.controller__ps5')
+const xbox = document.querySelector('.controller__xbox')
+const disItems = document.querySelectorAll('.discover__item')
+const js1000 = document.getElementById('js1000')
+const js1650 = document.getElementById('js1650')
+const js2700s = document.querySelectorAll('.js2700')
+const js3110s = document.querySelectorAll('.js3110')
+const js3960s = document.querySelectorAll('.js3960')
+const js4200s = document.querySelectorAll('.js4200')
+const js5530s = document.querySelectorAll('.js5530')
+const js7100 = document.querySelector('.js7100')
+const js7250 = document.querySelector('.js7250')
+const js7450 = document.querySelector('.js7450')
+
+window.addEventListener('scroll', () => {
+    let pos = window.scrollY;
+    ps5.style.transform = `rotate(${pos/20}deg)`;
+    xbox.style.transform = `rotate(-${pos/20}deg)`;
+    
+    console.log(pos)
+    
+    if (pos>200) {
+        disItems.forEach(item => {
+            item.classList.add('active')
+        })
+    }
+
+    if (pos>999) {
+        js1000.classList.add('active')
+    }
+
+    if (pos>1650) {
+        js1650.classList.add('active')
+    }
+
+    if (pos>2699) {
+        js2700s.forEach(item => {
+            item.classList.add('active')
+        })
+    }
+
+    if (pos>3110) {
+        js3110s.forEach(item => {
+            item.classList.add('active')
+        })
+    }
+
+    if (pos>4400) {
+        js3960s.forEach(item => {
+            item.classList.add('active')
+        })
+    }
+
+    if (pos>4600) {
+        js4200s.forEach(item => {
+            item.classList.add('active')
+        })
+    }
+
+    if (pos>5830) {
+        js5530s.forEach(item => {
+            item.classList.add('active')
+        })
+    }
+
+    if (pos>7600) {
+        js7100.classList.add('active')
+    }
+    
+    if (pos>7750) {
+        js7250.classList.add('active')
+    }
+    
+    if (pos>7900) {
+        js7450.classList.add('active')
+    }
+    
+})
